3c9ef1f74ff4ed94c9e77a6d62ec1761fe552f9a
Khushboopandit <khushboo17@navgurukul.org>
2018-12-08 18:33:05 +0530

    added doc folder

---
+++ docs/precache-manifest.2e31c6d320c61bac12c0eaebe96db71d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2999ca3886d776646afe",
+    "url": "/static/js/main.2999ca38.chunk.js"
+  },
+  {
+    "revision": "ace62ad78903889e8e0e",
+    "url": "/static/js/1.ace62ad7.chunk.js"
+  },
+  {
+    "revision": "2999ca3886d776646afe",
+    "url": "/static/css/main.a7d17ea0.chunk.css"
+  },
+  {
+    "revision": "eee7f4000be6f9dcea21768c6539d4c7",
+    "url": "/index.html"
+  }
+];
