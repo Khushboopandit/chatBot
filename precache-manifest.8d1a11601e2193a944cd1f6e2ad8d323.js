0b8af2da0020088f72c471fe06f42adf9bbef433
Khushboopandit <khushboo17@navgurukul.org>
2019-01-02 11:20:25 +0530

    Updates

---
+++ precache-manifest.8d1a11601e2193a944cd1f6e2ad8d323.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9506e10c0699cbe0f99b",
+    "url": "/chatBot/static/js/runtime~main.9506e10c.js"
+  },
+  {
+    "revision": "f606815e9fed0590363d",
+    "url": "/chatBot/static/js/main.f606815e.chunk.js"
+  },
+  {
+    "revision": "ace62ad78903889e8e0e",
+    "url": "/chatBot/static/js/1.ace62ad7.chunk.js"
+  },
+  {
+    "revision": "f606815e9fed0590363d",
+    "url": "/chatBot/static/css/main.847ea935.chunk.css"
+  },
+  {
+    "revision": "0ca4fe1b772501e653551e475b63e9a4",
+    "url": "/chatBot/index.html"
+  }
+];
